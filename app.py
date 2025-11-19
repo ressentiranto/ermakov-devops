83037ae55271e77a2f6aaa8564cae71a25145089
railwaytohell <kenzaburooe.luc@gmail.com>
2025-11-19 15:19:31 +0300

    version check index upd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "flask works; updated index;labels"
+    return "flask works; updated index;labels;vers check"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7245)
