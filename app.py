3b14c6eacc493ad7207c9a363223dece558856d1
railwaytohell <kenzaburooe.luc@gmail.com>
2025-11-19 17:30:19 +0300

    app changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "flask works; updated index;labels;vers check;v check 2;v ch3;v ch4"
+    return "flask works; updated index;labels;vers check;v check 2;v ch3;v ch4;hi"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7245)
